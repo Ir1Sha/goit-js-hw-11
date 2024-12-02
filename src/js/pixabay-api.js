a0127087d6bcda0a37149a84bd5cefae044463d2
ginny <ir.gonccharova.up@gmail.com>
2024-12-02 22:43:04 +0200

    modules added

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,11 @@
+export async function fetchImages(query) {
+    const API_KEY = '47416183-73bb296483032a9a16a802376';
+    const BASE_URL = 'https://pixabay.com/api/';
+    
+    const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}`);
+    if (!response.ok) {
+      throw new Error("Sorry, there are no images matching your search query. Please try again!");
+    }
+    
+    return await response.json();
+  }
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
