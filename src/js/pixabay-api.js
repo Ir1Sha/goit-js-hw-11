6de174f8605759de67f7ab0c38276fec818e06f6
ginny <ir.gonccharova.up@gmail.com>
2024-12-03 22:41:52 +0200

    api request added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,10 @@
 export async function fetchImages(query) {
     const API_KEY = '47416183-73bb296483032a9a16a802376';
     const BASE_URL = 'https://pixabay.com/api/';
+
+
     
-    const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}`);
+    const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`);
     if (!response.ok) {
       throw new Error("Sorry, there are no images matching your search query. Please try again!");
     }
--- src/main.js
+++ src/main.js
@@ -4,3 +4,40 @@ import iziToast from "izitoast";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
 
+import { fetchImages } from './js/pixabay-api.js';
+
+const form = document.querySelector('#search-form');
+const input = form.querySelector('input[name="searchQuery"]');
+const gallery = document.querySelector('.gallery');
+const btn = document.querySelector('.search-btn');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const query = input.value.trim();
+
+    if(!query) return;
+
+    try {
+        const data = await fetchImages(query);
+
+        if(data.hits.length === 0) {
+            iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight',
+            });
+            return;
+        }
+
+        console.log(data.hits);
+    } 
+
+    catch(error) {
+        iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight',
+        });
+    }
+});
+
+
